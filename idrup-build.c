4b4561e35d38f11469f093214fed7fcf161f5bb5
Armin Biere <armin.biere@gmail.com>
2023-12-07 07:58:00 +0100

    build info

---
+++ idrup-build.c
@@ -0,0 +1,7 @@
+#include "idrup-build.h"
+#include "idrup-config.h"
+
+const char * const idrup_build = COMPILER;
+const char * const idrup_compiler = GITID;
+const char * const idrup_gitid = VERSION;
+const char * const idrup_version = BUILD;
